ff26078e576d9a86281acc40b9d76a1a2999f610
Miguelangel <silvermiguel96@gmail.com>
2019-05-07 10:04:57 -0500

    Desafio:Terminar resolvers

--- resolvers.js
+++ resolvers.js
@@ -11,15 +11,27 @@ const resolvers = {
         id: 2,
         titulo: 'Segundo curso',
         descripcion: 'Esta es la descripcion del curso Segundo'
-      },
-      ]
+      }]
     }
   },
   Curso: {
     profesor: () => {
       return {
-        nombre: 'Miguel'
+        nombre: 'Miguel',
+        nacionalidad: 'Colombiana'
       }
+    },
+    comentarios: () => {
+      return [{
+        id: 1,
+        nombre: 'Comentario uno',
+        cuerpo: 'Cuerpo uno'
+      },
+      {
+        id: 2,
+        nombre: 'Comentario dos',
+        cuerpo: 'Cuerpo dos'
+      }]
     }
   }
 }
